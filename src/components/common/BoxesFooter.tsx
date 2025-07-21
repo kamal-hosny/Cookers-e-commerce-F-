5409710660045bc99289e6a441329217bc48f139
kamal <ixonhosny@gmail.com>
2025-07-21 03:24:16 +0300

    done

--- src/components/common/BoxesFooter.tsx
+++ src/components/common/BoxesFooter.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 const BoxesFooter = () => {
   const navigate = useNavigate();
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   const card = [
     {
